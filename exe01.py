47683ec3be47217726f07997e155eb1a9ed15a88
Matheus <matheus0509@live.com>
2025-04-02 14:43:26 -0300

    Alteração exe04

--- exe01.py
+++ exe01.py
@@ -1,15 +1,15 @@
 litrocombustivel= float(input("Quantos litros de combustivel voce colocou? \n"))
 combustivel = input("Digite seu combustivel  \n"
-                    "G para Gasolina\n"
-                    "E para Etanol\n")
+                    "G or g para Gasolina\n"
+                    "E or e para Etanol\n")
 
 vGas = 6.5
 vEta = 4.9
 
-if combustivel == 'G':
+if combustivel == 'G' or 'g':
         valorgasolina = litrocombustivel * vGas
         print(f"Você abasteceu {litrocombustivel} e gastou {valorgasolina} de Gasolina\n")
 else:
-    if combustivel == 'E':
+    if combustivel == 'E' or 'e':
         valoretanol = litrocombustivel * vEta
         print(f"Você abasteceu {litrocombustivel} e gastou {valoretanol} de etanol\n")
