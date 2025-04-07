85ece8645166627a6c1b13c513c7dea986ff8b1a
Matheus <matheus0509@live.com>
2025-04-07 14:06:49 -0300

    Desafio 01

--- exe03.py
+++ exe03.py
@@ -1,13 +1,19 @@
+#Desagio 01
 hora1 = int(input(" Digite um hora ate 12h\n"))
-minuto1 =float(input("Digite quantos minutos\n"))
+minuto1 =int(input("Digite quantos minutos\n"))
 
 hora2 = int(input(" Digite um hora ate 24h\n"))
-minuto2 =float(input("Digite quantos minutos\n"))
+minuto2 =int(input("Digite quantos minutos\n"))
 
 somahora = hora1 + hora2
 somaminuto = (minuto1 + minuto2)
 
-
 if somahora > 12:
-    somatotal = somatotal - 12
-p   rint(f"{somatotal}")
+    somahora = somahora -12
+if somahora> 24:
+    somahora = somahora - 24
+if somaminuto >= 60:
+    somaminuto = somaminuto - 60
+    somahora = somahora + 1
+print(f"{somahora}:{somaminuto}")
+
