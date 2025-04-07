09cf4b961578c25df17a222bb8743016ba7608e7
Matheus <matheus0509@live.com>
2025-04-07 14:21:38 -0300

    exe04

---
+++ exe04.py
@@ -0,0 +1,28 @@
+mes = int(input("Digite o mês do ano\n"))
+
+if mes == 1:
+    print("Janeiro\n")
+if mes == 2:
+    print("Fevereiro\n")
+if mes == 3:
+     print("Março\n")
+if mes == 4:
+     print("Abril\n")
+if mes == 5:
+     print("Maio\n")
+if mes == 6:
+     print("Junho\n")
+if mes == 7:
+    print("Julho\n")
+if mes == 8:
+    print("Agosto\n")
+if mes == 9:
+    print("Setembro\n")
+if mes == 10:
+    print("Outubro\n")
+if mes ==11:
+     print("novembro\n")
+if mes == 12:
+    print("Dezembro\n")
+else :
+    print("Numero invalido!")
